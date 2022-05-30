b29e72522b24386ba00a908ef3eff6720d095e03
jonlachmann <jon@lachmann.nu>
2022-05-30 23:52:51 +0200

    Minor adjustments

--- R/quantiles.R
+++ R/quantiles.R
@@ -1,6 +1,6 @@
 #' Calculate the quantiles and mean of rows in a matrix
 #' @param mat The matrix to use
-#' @param quantiles The quantiles to calculate, defaults to 0.05 and 0.95 (i.e. 90%)
+#' @param quantiles The quantiles to calculate, defaults to 0.05 and 0.95 (i.e. 90\%)
 #' @param mean Should the mean also be calculated? Defaults to TRUE
 #' @return A list containing the mean of each row, together with the low and high quantiles selected
 #' @export
@@ -14,11 +14,11 @@ rowQuantmean <- function (mat, quantiles=c(0.05, 0.95), mean=TRUE) {
 
 #' Function for calculating the quantiles and mean of many matrices in a list
 #' @param matlist The list of matrices to use
-#' @param quantiles The quantiles to calculate, defaults to 0.05 and 0.95 (i.e. 90%)
+#' @param quantiles The quantiles to calculate, defaults to 0.05 and 0.95 (i.e. 90\%)
 #' @param mean Should the mean also be calculated? Defaults to TRUE
 #' @return A list containing the mean per element over all the matrices, together with the low and high quantiles selected
 #' @export
-matrix_quantmean <- function (matlist, quantiles=c(0.05, 0.95), mean=TRUE) {
+matrixQuantmean <- function (matlist, quantiles=c(0.05, 0.95), mean=TRUE) {
   mat_all <- matrix(NA, nrow(matlist[[1]]) * ncol(matlist[[1]]), length(matlist))
   count <- 0
   for (i in seq_along(matlist)) {
